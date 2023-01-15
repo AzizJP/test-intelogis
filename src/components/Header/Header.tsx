106e034c5d0c15a38ed3a80d77292670ea725e1d
Aziz <t1-zer@yandex.ru>
2023-01-15 15:21:28 +0300

    feat: add Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,11 @@ import {FC, memo} from 'react';
 import './Header.scss';
 
 const Header: FC = memo(() => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <h1 className="header__title">Заявки на перевозку</h1>
+    </header>
+  );
 });
 
 export default Header;
